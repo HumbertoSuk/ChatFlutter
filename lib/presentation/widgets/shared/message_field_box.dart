33b163554493ab2d737a35d1844ddabb6b174885
July Suk <humbertoxd25@gmail.com>
2024-03-07 22:03:48 -0800

    text field box botones y focus

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,17 +5,44 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textcontroler = TextEditingController();
+    final focus = FocusNode();
+
     final UnderlineInputBorder outLineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(20));
+
+    final decoration = InputDecoration(
+        enabledBorder: outLineInputBorder,
+        focusedBorder: outLineInputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send),
+          onPressed: () {
+            final text = textcontroler.value.text;
+            print("boton de envio $text");
+            textcontroler.clear();
+          },
+        ));
+    //decoration: decoration
     return TextFormField(
-        decoration: InputDecoration(
-            enabledBorder: outLineInputBorder,
-            focusedBorder: outLineInputBorder,
-            filled: true,
-            suffixIcon: IconButton(
-              icon: const Icon(Icons.send),
-              onPressed: () {},
-            )));
+        decoration: decoration,
+        controller: textcontroler,
+        focusNode: focus,
+        onFieldSubmitted: (value) {
+          print("sudmit:  $value");
+          textcontroler.clear();
+          focus.requestFocus();
+        });
   }
+
+  UnderlineInputBorder outLineInputBorder() => UnderlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(20));
+
+  InputDecoration setDecoration({required InputBorder}) => InputBorder(
+      enabledBorder: outLineInputBorder,
+      focusedBorder: outLineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(icon: const Icon(Icons.send), onPressed: () {}));
 }
